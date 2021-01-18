a242b6196b1b291e70ba2195c1016da6b480734d
ahmedgadit <ahmed.gadit786@gmail.com>
2021-01-18 18:21:19 +0500

    route clear

---
+++ src/views/weatherdetail.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="box">
+		<p class="text">Dashboard</p>
+		<div class="row">
+			<div class="column">
+				<div class="card">
+					<h3>Card 1</h3>
+					<p>Some text</p>
+					<p>Some text</p>
+				</div>
+			</div>
+
+			<div class="column">
+				<div class="card">
+					<h3>Card 2</h3>
+					<p>Some text</p>
+					<p>Some text</p>
+				</div>
+			</div>
+
+			<div class="column">
+				<div class="card">
+					<h3>Card 3</h3>
+					<p>Some text</p>
+					<p>Some text</p>
+				</div>
+			</div>
+
+			<div class="column">
+				<div class="card">
+					<h3>Card 4</h3>
+					<p>Some text</p>
+					<p>Some text</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+
+</script>
